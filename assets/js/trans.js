9d85ae073f2c77e3a280510d5c41dad49d8032cc
thaisborges <thaisferreirab00@gmail.com>
2024-08-01 23:51:43 -0300

    teste

--- assets/js/trans.js
+++ assets/js/trans.js
@@ -1,10 +1,9 @@
 
 // script.js
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) { // Ajuste o valor conforme necessário
-        navbar.classList.add('transparent');
-    } else {
-        navbar.classList.remove('transparent');
-    }
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('navbar');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
 });
+
