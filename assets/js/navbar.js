733f56ea6b69765cded5ecd706e17ce494db1b25
thaisborges <thaisferreirab00@gmail.com>
2024-08-07 17:36:30 -0300

    mudanças no footer e header

---
+++ assets/js/navbar.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    const navbar = document.querySelector('.navbar');
+
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+});
