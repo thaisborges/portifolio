96410a07f70120eedfa52e7228ad575c8c0664b7
thaisborges <thaisferreirab00@gmail.com>
2024-08-10 18:32:58 -0300

    ajustes no layout

--- assets/js/trans.js
+++
@@ -1,9 +0,0 @@
-
-// script.js
-const menuIcon = document.getElementById('menu-icon');
-const navbar = document.querySelector('navbar');
-
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
-
